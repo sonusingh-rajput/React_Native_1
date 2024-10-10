c3da53b01ae1d890e2d7a2f1aa9d5a42e88ff539
Sonu Singh <sonusinghrajput270@gmail.com>
2024-10-10 12:27:41 +0530

    Crate A Card

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
-import DynamicContent from "../components/DynamicContent";
+import CardComponents from "../components/CardComponents";
 
 const index = () => {
   return (
     <View>      
-      <DynamicContent />
+      <CardComponents />
     </View>
   );
 };
